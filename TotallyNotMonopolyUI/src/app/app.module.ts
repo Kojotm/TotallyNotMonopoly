a4f5affcd2d452412dc387d5825567d7affc70ce
Csékei Róbert <wn4th8@inf.elte.hu>
2021-10-07 20:52:24 +0200

    Design the playing field
    Created a grid-like test playing field (12x12)

--- TotallyNotMonopolyUI/src/app/app.module.ts
+++ TotallyNotMonopolyUI/src/app/app.module.ts
@@ -7,10 +7,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GameTableComponent } from './game-table/game-table.component';
 
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatDividerModule } from '@angular/material/divider'; 
+import { MatButtonModule } from '@angular/material/button'; 
 
 
 const MaterialComponents = [
   MatGridListModule
+  MatGridListModule,
+  MatCardModule,
+  MatDividerModule,
+  MatButtonModule,
 ]
 
 
--- TotallyNotMonopolyUI/src/app/game-table/game-table.component.ts
+++ TotallyNotMonopolyUI/src/app/game-table/game-table.component.ts
@@ -1,5 +1,19 @@
 import { ArrayType } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface Tile {
+  name: string;
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+export interface Player {
+  number: number;
+  money: number;
+  properties: string[];
+}
 
 @Component({
   selector: 'app-game-table',
@@ -7,9 +21,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-table.component.scss']
 })
 export class GameTableComponent implements OnInit {
-  public rows: number[] = Array(144).fill(0);
-
-  constructor() { }
+  tiles: Tile[] = [
+    { name:"top-left", color:"rgb(82, 80, 80)", cols:2, rows:2, text:"top left corner tile"},
+    { name:"top-row", color:"grey", cols:9, rows:2, text:"top row tiles" },
+    { name:"top-right", color:"rgb(82, 80, 80)", cols:2, rows:2, text:"top right corner tile"},
+    { name:"left-col", color:"grey", cols:2, rows:9, text:"left col tiles"},
+    { name:"filler", color:"white", cols:9, rows:9, text:"Totally not monopoly board"},
+    { name:"right-col", color:"grey", cols:2, rows:9, text:"right col tiles"},
+    { name:"bottom-left", color:"rgb(82, 80, 80)", cols:2, rows:2, text:"bottom left corner tile"},
+    { name:"bottom-row", color:"grey", cols:9, rows:2,text:"bottom row tiles"},
+    { name:"bottom-right", color:"rgb(82, 80, 80)", cols:2, rows:2, text:"bottom right corner tile"},
+  ];
+  players: Player[] = [
+    {number:1, money:505000,properties:["Property 1","Property 2", "Property 3"]},
+    {number:2, money:5000,properties:["Property 1","Property 2", "Property 3"]},
+    {number:3, money:5000,properties:["Property 1","Property 2", "Property 3"]},
+    {number:4, money:5000,properties:["Property 1","Property 2", "Property 3"]},
+  ];
+  
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
