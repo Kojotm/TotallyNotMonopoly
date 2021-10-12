e81ab55adfa026d58c5f270ee2a8655721b52439
martin <fih29b@inf.elte.hu>
2021-10-12 19:03:05 +0200

    Create  PlayerService and PlayerController class
    Create the first version of the PlayerService and PlayerController classes. To be able to communicate the frontend with the backend.
    We create some methods to use in the future.

---
+++ TotallyNotMonopolyAPI/Services/PlayerService.cs
@@ -0,0 +1,79 @@
+﻿using Model;
+using System;
+
+namespace Services
+{
+    public class PlayerService
+    {
+
+        public Player[] CreateRamdonPlayers()
+        {
+            Player[] players = new Player[4];
+            
+            Player player1 = new Player();
+            player1.Id = 1;
+            player1.Money = 50000;
+            player1.Name = "Martin";
+
+            players[0] = player1;
+
+            Player player2 = new Player();
+            player2.Id = 2;
+            player2.Money = 50000;
+            player2.Name = "Marcell";
+
+            players[1] = player2;
+
+            Player player3 = new Player();
+            player1.Id = 3;
+            player1.Money = 50000;
+            player1.Name = "Daniel";
+
+            players[2] = player3;
+
+            Player player4 = new Player();
+            player4.Id = 4;
+            player4.Money = 50000;
+            player4.Name = "Robert";
+
+            players[3] = player4;
+
+            return players;
+        }
+
+        public Player BuyProperty(Player player, Property property)
+        {
+            if (player.Money >= property.cost)
+            {
+                player.properties.Add(property);
+            }
+            else
+            {
+                return player;
+            }
+
+            return player;
+        }
+
+        public Player SellProperty(Player player, Property property)
+        {
+            property.Owner = null;
+            player.properties.Remove(property);
+
+            return player;
+        }
+
+        public Player PayRent(Player playerOwner, Player playerTenant, Property property)
+        {
+            // To Do
+            return playerTenant;
+        }
+
+        public Player PassGo(Player player)
+        {
+            player.Money = player.Money + 500;
+
+            return player;
+        }
+    }
+}
---
+++ TotallyNotMonopolyAPI/WebAPI/Controllers/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    public class PlayerController : Controller
+    {
+        PlayerService playerService = new PlayerService();
+
+        [HttpGet]
+        public ActionResult<Player[]> GetPlayers()
+        {
+            Player[] players = playerService.CreateRamdonPlayers();
+            return new ActionResult<Player[]>(players);
+        }
+
+        // GET: PlayerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PlayerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PlayerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlayerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PlayerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlayerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PlayerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
