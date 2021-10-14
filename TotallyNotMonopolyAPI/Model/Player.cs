7fcf16b4aff7f19fac49c3884ecf52df7025785a
fih29b <91495180+fih29b@users.noreply.github.com>
2021-10-14 12:20:08 +0200

    Update Player.cs

--- TotallyNotMonopolyAPI/Model/Player.cs
+++ TotallyNotMonopolyAPI/Model/Player.cs
@@ -11,6 +11,6 @@ namespace Model
         public int Id { get; set; }
         public string Name { get; set; }
         public int Money { get; set; } = 1500;
-        public List<Property> properties { get; set; }
+        public List<Property> Properties { get; set; }
     }
 }
