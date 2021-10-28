bee9d9c98c5b2437f4fd735289d6def2d4ea4333
Havasdi Dániel <nopsic@inf.elte.hu>
2021-10-28 19:47:54 +0200

    Add migrations

---
+++ TotallyNotMonopolyAPI/Persistence/Migrations/20211028124252_ChangeColumnNames.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class ChangeColumnNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cost",
+                table: "TableCards",
+                newName: "Rent");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UpgradeCost",
+                table: "TableCards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Color",
+                table: "TableCards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Rent",
+                table: "TableCards",
+                newName: "Cost");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UpgradeCost",
+                table: "TableCards",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "TableCards",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
