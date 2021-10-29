3138559724a7fb82242c414b323039d397f3cc53
Marcell Kajati <pylobf@inf.elte.hu>
2021-10-29 19:32:25 +0200

    Made Field Controller

---
+++ TotallyNotMonopolyAPI/WebAPI/Controllers/FieldsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Model.Cards;
+using Persistence;
+
+namespace WebAPI.Controllers
+{
+    public class FieldsController : Controller
+    {
+        private readonly GameTableContext _context;
+
+        public FieldsController(GameTableContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Fields
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cards.ToListAsync());
+        }
+
+        // GET: Fields/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @field = await _context.Cards
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@field == null)
+            {
+                return NotFound();
+            }
+
+            return View(@field);
+        }
+
+        // GET: Fields/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Fields/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Color,Image,Rent,Value,Level,UpgradeCost,Col,Row,Type,SetNumber")] Field @field)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@field);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@field);
+        }
+
+        // GET: Fields/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @field = await _context.Cards.FindAsync(id);
+            if (@field == null)
+            {
+                return NotFound();
+            }
+            return View(@field);
+        }
+
+        // POST: Fields/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Color,Image,Rent,Value,Level,UpgradeCost,Col,Row,Type,SetNumber")] Field @field)
+        {
+            if (id != @field.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@field);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FieldExists(@field.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@field);
+        }
+
+        // GET: Fields/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @field = await _context.Cards
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@field == null)
+            {
+                return NotFound();
+            }
+
+            return View(@field);
+        }
+
+        // POST: Fields/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @field = await _context.Cards.FindAsync(id);
+            _context.Cards.Remove(@field);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FieldExists(int id)
+        {
+            return _context.Cards.Any(e => e.Id == id);
+        }
+    }
+}
